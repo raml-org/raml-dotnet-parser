378ace0613e0402ff663920a18165ca6184ea6c3
pedrow <mail_pedro@yahoo.com.ar>
2015-02-12 09:10:26 -0300

    deleted obsolete file

--- Raml.Parser/test.js
+++
@@ -1,8 +0,0 @@
-var raml = require('raml-parser');
-
-  raml.loadFile('\\desarrollo\\_raml\\Raml.Parser\\Raml.VisualStudio.Tools.Tests\\movies.raml','\\desarrollo\\_raml\\Raml.Parser\\Raml.VisualStudio.Tools.Tests\\')
-  .then( function(data) {
-    console.log(data);
-  }, function(error) {
-    console.log('Error parsing: ' + error);
-  });
