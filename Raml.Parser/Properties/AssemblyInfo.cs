85677949e5cb66b545844eeda0d034dc6ff507fb
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-07-29 15:20:49 -0300

    updated version

--- Raml.Parser/Properties/AssemblyInfo.cs
+++ Raml.Parser/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.101")]
-[assembly: AssemblyFileVersion("0.8.101")]
-[assembly: AssemblyInformationalVersion("0.8.101")]
+[assembly: AssemblyVersion("0.8.102")]
+[assembly: AssemblyFileVersion("0.8.102")]
+[assembly: AssemblyInformationalVersion("0.8.102")]
