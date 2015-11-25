ae47815ad5fa5006e05cb5bac36a877c280e14f8
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-11-25 15:10:03 -0300

    security schemes

--- source/Raml.Parser/Expressions/SecuritySchemeDescriptor.cs
+++ source/Raml.Parser/Expressions/SecuritySchemeDescriptor.cs
@@ -9,5 +9,12 @@ namespace Raml.Parser.Expressions
 		public IDictionary<string, Parameter> QueryParameters { get; set; }
 
 		public IDictionary<string, Response> Responses { get; set; }
+
+        public IDictionary<string, Parameter> QueryString { get; set; } // TODO: check
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
 	}
 }
