81f7e0edf4e1179b10601fa6dc6aca3beb0dd095
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-07-11 13:34:35 -0300

    add library prefix to tratis and security schemes #15

--- source/Raml.Parser.Tests/RamlParserTests.cs
+++ source/Raml.Parser.Tests/RamlParserTests.cs
@@ -379,5 +379,27 @@ namespace Raml.Parser.Tests
             Assert.AreEqual(14, raml.Traits.Count());
             Assert.AreEqual("error[]", raml.Traits.First()["Error400"].Responses.First().Body["application/json"].Type);
         }
+
+        [Test]
+        public async Task ShouldParseLibraryTraitsWithPrefix()
+        {
+            var parser = new RamlParser();
+            var raml = await parser.LoadAsync("Specifications/libs.raml");
+
+            Assert.IsTrue(raml.Traits.Any(t => t.ContainsKey("Lib.filtered_by_month")));
+            Assert.AreEqual(2, raml.Traits.First(t => t.Keys.Any(k => k == "Lib.filtered_by_month"))["Lib.filtered_by_month"].QueryParameters.Count);
+            Assert.AreEqual("Lib.filtered_by_month", raml.Resources.First().Methods.First().Is.First());
+        }
+
+        [Test]
+        public async Task ShouldParseLibrarySecuritySchemasWithPrefix()
+        {
+            var parser = new RamlParser();
+            var raml = await parser.LoadAsync("Specifications/libs.raml");
+
+            Assert.IsTrue(raml.SecuritySchemes.Any(s => s.ContainsKey("Lib.oauth_2_0")));
+            Assert.AreEqual("OAuth 2.0", raml.SecuritySchemes.First(t => t.Keys.Any(k => k == "Lib.oauth_2_0"))["Lib.oauth_2_0"].Type.First().Key);
+            Assert.AreEqual("Lib.oauth_2_0", raml.Resources.First().Methods.First().SecuredBy.First());
+        }
     }
 }
