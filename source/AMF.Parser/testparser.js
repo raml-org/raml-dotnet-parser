2ed6eecb8d8059cce15ce8c96229be5277996c5f
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-05-17 15:09:06 -0300

    missing amf files

--- source/AMF.Parser/testparser.js
+++ source/AMF.Parser/testparser.js
@@ -5,23 +5,23 @@ var util = require('util')
 
 console.log('Start');
 
-// var filepath = "file:///desarrollo/mulesoft/raml-dotnet-parser-2/source/AMF.Parser.Tests/specs/chinook-v1.raml"
-// parser.parse('raml', filepath, function (m) {
-//     console.log(m);
-// });
-
-// var filepath = "file:///desarrollo/mulesoft/raml-dotnet-parser-2/source/AMF.Parser.Tests/specs/arrayTypes.raml"
-// parser.parse('raml', filepath, function (m) {
-//     console.log(m);
-// });
-
-var filepath = "file:///desarrollo/mulesoft/raml-dotnet-parser-2/source/AMF.Parser.Tests/specs/typeExpressions.raml"
+var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/chinook-v1.raml"
 parser.parse('raml', filepath, function (m) {
     console.log(m);
 });
 
+// var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/arrayTypes.raml"
+// parser.parse('raml', filepath, function (m) {
+//     console.log(m);
+// });
 
-// var filepath = "file:///desarrollo/mulesoft/raml-dotnet-parser-2/source/AMF.Parser.Tests/specs/oas/yaml/api-with-examples.yaml"
+// var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/typeExpressions.raml"
+// parser.parse('raml', filepath, function (m) {
+//     console.log(m);
+// });
+
+
+// var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/oas/yaml/api-with-examples.yaml"
 // parser.parse('oas', filepath, function (m) {
 //     console.log(m);
 // });
