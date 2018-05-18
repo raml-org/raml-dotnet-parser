5490a5f368e645169cfda796d079e56655021ba8
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-05-18 09:56:16 -0300

    back to amf 1.1.0

--- source/AMF.Parser/testparser.js
+++ source/AMF.Parser/testparser.js
@@ -1,14 +1,19 @@
 
 
 var parser = require('parser');
-var util = require('util')
+//var util = require('util')
 
 console.log('Start');
 
-var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/chinook-v1.raml"
-parser.parse('raml', filepath, function (m) {
-    console.log(m);
-});
+try {
+    var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/chinook-v1.raml"
+    parser.parse('raml', filepath, function (m) {
+        console.log(m);
+    });
+}
+catch(ex){
+    console.log(ex);
+}
 
 // var filepath = "file:///desarrollo/mulesoft/amf-dotnet-parser/source/AMF.Parser.Tests/specs/arrayTypes.raml"
 // parser.parse('raml', filepath, function (m) {
