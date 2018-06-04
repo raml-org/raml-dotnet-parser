75ad79e4f8f81a5c2f3cde65202b8472139c9d92
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-06-04 13:33:35 -0300

    add unit çtest

---
+++ source/AMF.Parser.Tests/SpecWithErrorsTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AMF.Parser;
+using System.Linq;
+using AMF.Parser.Model;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SpecWithErrorsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AggregateException))]
+        public void error_spec_check()
+        {
+            var parser = new AmfParser();
+            Assert.ThrowsException<AggregateException>(() =>
+            {
+                var model = parser.Load("./specs/oas/yaml/petstore-with-external-docs.yaml").Result;
+            });
+        }
+    }
+}
