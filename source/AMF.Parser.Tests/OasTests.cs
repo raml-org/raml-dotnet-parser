c46dd2e5e75abe1b182fbee8559a567572412bca
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-09-03 11:32:40 -0300

    oas tests

---
+++ source/AMF.Parser.Tests/OasTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AMF.Parser;
+using System.Linq;
+using AMF.Parser.Model;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class OasTests
+    {
+        private AmfParser parser;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            parser = new AmfParser();
+        }
+
+        [TestMethod]
+        public void Endpoints_count()
+        {
+            var model = parser.Load("./specs/oas/yaml/talkvotes.oas2.yaml").Result;
+            Assert.AreEqual(2, model.WebApi.EndPoints.Count());
+        }
+
+        [TestMethod]
+        public void Uber_name_check()
+        {
+            var model = parser.Load("./specs/oas/json/uber.json").Result;
+            Assert.AreEqual("Uber API", model.WebApi.Name);
+        }
+    }
+}
