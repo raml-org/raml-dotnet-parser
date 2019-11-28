6cdd5cca4f0f5ac069411a6e7a7f66b8a2f6bcc5
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-11-28 11:16:34 -0300

    Update to AMF 4.0.2

--- source/RAML.Parser/Properties/AssemblyInfo.cs
+++ source/RAML.Parser/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RAML.Parser")]
-[assembly: AssemblyDescription("RAML and OAS (swagger) parser for .Net")]
+[assembly: AssemblyDescription("RAML and OAS (swagger/OpenAPI) parser for .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MuleSoft")]
 [assembly: AssemblyProduct("RAML.Parser")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
